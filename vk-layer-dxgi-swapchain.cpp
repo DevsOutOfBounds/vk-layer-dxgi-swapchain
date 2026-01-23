93aa8b3abc184c826e50b56c0ccb2a70df8b496f
ZilverBlade <ZilverBlade@DESKTOP-11P7QO1>
2026-01-23 17:38:56 +0100

    Add project files.

---
+++ vk-layer-dxgi-swapchain.cpp
@@ -0,0 +1,12 @@
+﻿// vk-layer-dxgi-swapchain.cpp : Defines the entry point for the application.
+//
+
+#include "vk-layer-dxgi-swapchain.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
